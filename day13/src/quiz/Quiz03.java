b206997ed2646cd139f19e9a65663f4b4d34cfcc
Eric han <han21ch9@gmail.com>
2023-11-01 06:22:54 +0900

    day13

--- day13/src/quiz/Quiz03.java
+++ day13/src/quiz/Quiz03.java
@@ -6,15 +6,14 @@ class TV{
 	// TV의 속성을 필드로 작성하세요
 	// 전원, 채널, 볼륨
 	boolean power;
+	boolean mute;
 	int channel;
 	int vol;
 	
 	// TV의 기능을 메서드로 정의하세요
 	// 전원 on/off , 채널 올림/내림, 볼륨 올림/내림, TV상태 출력, 음소거
-	TV(int channelUp, int channelDown){
-		this.channelUp = channel + 1;
-	}
-	
+		
+		
 	
 	// 채널은 일정 범위안에서 cycle 형식으로 순환되어야 합니다
 	// 볼륨은 일정 범위안에서 최소 최대치를 넘길 수 없습니다.
@@ -22,6 +21,51 @@ class TV{
 	// 대부분의 기능은 전원이 켜져있는 상태에서만 작동해야 합니다.
 	void turn() {
 		power = !power;
+		if(power) {
+			System.out.println("전원이 켜졌습니다.");
+		}
+		else {
+			System.out.println("전원이 꺼졌습니다.");
+		}
+	}
+	void channelUp() {
+		channel ++;
+		if(channel > 20) {
+			System.out.println("현재 채널 : " + channel);
+		}
+	}
+	void channelDown() {
+		channel --;
+		if(channel < 1) {
+			System.out.println("현재 채널 : " + channel);
+		}
+	}
+	void mutee() {
+		mute = !mute;
+		if(mute) {
+			System.out.println("음소거 활성화");
+		}
+		else {
+			System.out.println("음소거 비활성화");
+		}
+	}
+	void volUp() {
+		if(mute) {
+			mutee();
+		}
+		if(vol < 40) {
+			vol ++;
+		}
+		System.out.println("현재 볼륨 : " + vol);
+	}
+	void volDown() {
+		if(mute) {
+			mutee();
+		}
+		if(vol > 0) {
+			vol --;
+		}
+		System.out.println(" 현재 볼륨 :" + vol);
 	}
 	
 	// 생성자에게 객체 생성 시 초기값을 지정하세요
@@ -51,6 +95,10 @@ public class Quiz03 {
 				tv.channelDown();
 			case 5:
 				tv.volUp();
+			case 4:
+				tv.volDown();
+			case 3:
+				tv.mute;
 				
 			case 0:
 				break LOOP;
